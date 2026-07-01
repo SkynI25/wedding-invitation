c4300698d566ad00cd356736a45442e1e40dc89c
Skyni25 <rlxo1651@naver.com>
2026-07-01 17:38:15 +0900

    first commit

---
+++ src/images/index.ts
@@ -0,0 +1,30 @@
+import coverImage from "./cover.jpg"
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
+import image5 from "./image5.jpg"
+import image6 from "./image6.jpg"
+import image7 from "./image7.jpg"
+import image8 from "./image8.jpg"
+import image9 from "./image9.jpg"
+
+/**
+ * 메인 커버 이미지
+ */
+export const COVER_IMAGE = coverImage
+
+/**
+ * 갤러리에 표시될 이미지 목록
+ */
+export const GALLERY_IMAGES = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+]
