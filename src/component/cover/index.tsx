38506b86182f7db7e0c0114a1e6a081769630b9a
skyni25 <rlxo1651@naver.com>
2026-07-03 12:56:23 +0900

    cover change

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -27,32 +27,36 @@ const DAY_OF_WEEK = [
 export const Cover = () => {
   return (
     <LazyDiv className="card cover">
-      {/* 상단 날짜 표시 */}
-      <div className="wedding-date">
-        {WEDDING_DATE.format("YYYY")}
-        <div className="divider" />
-        {WEDDING_DATE.format("MM")}
-        <div className="divider" />
-        {WEDDING_DATE.format("DD")}
-      </div>
-      {/* 요일 표시 (영어) */}
-      <div className="wedding-day-of-week">
-        {DAY_OF_WEEK[WEDDING_DATE.day()]}
-      </div>
-      {/* 커버 이미지 */}
+      {/* 커버 이미지 (전체 화면을 채우고 텍스트가 그 위에 오버레이됨) */}
       <div className="image-wrapper">
         <img src={COVER_IMAGE} alt="sample" />
+
+        {/* 상단 오버레이: 날짜 및 요일 */}
+        <div className="overlay overlay-top">
+          <div className="wedding-date">
+            {WEDDING_DATE.format("YYYY")}
+            <div className="divider" />
+            {WEDDING_DATE.format("MM")}
+            <div className="divider" />
+            {WEDDING_DATE.format("DD")}
+          </div>
+          <div className="wedding-day-of-week">
+            {DAY_OF_WEEK[WEDDING_DATE.day()]}
+          </div>
+        </div>
+
+        {/* 하단 오버레이: 이름 및 예식 정보 */}
+        <div className="overlay overlay-bottom">
+          <div className="subtitle">Save the date for the wedding of</div>
+          <div className="names">
+            {GROOM_FULLNAME}
+            <div className="divider" />
+            {BRIDE_FULLNAME}
+          </div>
+          <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
+          <div className="info">{LOCATION}</div>
+        </div>
       </div>
-      <div className="subtitle">Save the date for the wedding of</div>
-      {/* 이름 표시 */}
-      <div className="names">
-        {GROOM_FULLNAME}
-        <div className="divider" />
-        {BRIDE_FULLNAME}
-      </div>
-      {/* 예식 정보 (포맷팅된 날짜 및 장소) */}
-      <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
-      <div className="info">{LOCATION}</div>
     </LazyDiv>
   )
 }
